439a711638114fa269b54f2ffc2558f8e535b54e
AdriVelasco2 <isadri35@hotmail.com>
2023-10-10 15:12:43 +0200

    CSS Formulario

--- Grupo5ClonInsta-main/client/src/forms/LoginForm/LoginForm.jsx
+++ Grupo5ClonInsta-main/client/src/forms/LoginForm/LoginForm.jsx
@@ -36,33 +36,29 @@ function LoginForm() {
   }
   
     return (
-      <div>
-        <h2>Login</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label>email:</label>
-            <input
-              type="text"
+       
+    <div class="flex items-center justify-center h-screen w-screen bg-gray-900">
+    <form class="bg-white shadow-md rounded px-8 py-8 pt-8" onSubmit={handleSubmit}>
+        <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="email">Email</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" value="" type="text" placeholder="John" type="text"
               value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div>
-            <label>password:</label>
-            <input
-              type="password"
+              onChange={(e) => setEmail(e.target.value)}/>
+        </div>
+        <div class="mb-6">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="password">Password</label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"    type="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
-          <button type="submit" onSubmit={handleSubmit}>Log In</button>
-        </form>
-
-      {/* Agrega un enlace a la página de recuperación de contraseña */}
-      <p>
-        ¿Olvidaste tu contraseña? <a href="/forgot-password">Recupérala aquí</a>
-      </p>
-    </div>
+              onChange={(e) => setPassword(e.target.value)} placeholder="********"/>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit" onSubmit={handleSubmit}>
+                Sign In
+            </button>
+            <a class="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="/forgot-password">Forgot Password?</a>
+        </div>
+    </form>
+</div>
     );
   }
   
