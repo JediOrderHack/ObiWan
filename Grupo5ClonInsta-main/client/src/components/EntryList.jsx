05f7e4351984fd7d61bcd83cdf772ac4baba2a1e
isimarsal <isimarsal@gmail.com>
2023-10-12 12:42:40 +0200

    entradas mostrandose

--- Grupo5ClonInsta-main/client/src/components/EntryList.jsx
+++ Grupo5ClonInsta-main/client/src/components/EntryList.jsx
@@ -1,71 +1,64 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import UserAvatar from "./UserAvatar";
-import EntryLikes from "./EntryLikes";
-import EntryComment from "./EntryComments";
-import { getToken } from "../utils/getToken";
-import ViewComments from "./ViewComments";
 
-const IMAGES_URL = "http://localhost:3000/images";
+const IMAGES_URL = "http://localhost:3000/uploads";
 
-function EntryList({ entriesProp }) {
+function EntryList() {
   const [entries, setEntries] = useState([]);
   const [loading, setLoading] = useState(false);
-  const userAuthToken = getToken();
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/entries", {
-          timeout: 15000,
-        });
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://localhost:3000/entries", {
+        params: {
+          search,
+        },
+        timeout: 15000,
+      });
 
-        if (response.data.entries && Array.isArray(response.data.entries)) {
-          setEntries(response.data.entries.reverse());
-        } else {
-          console.error(
-            "La respuesta del servidor no contiene un array de entradas:",
-            response.data
-          );
-        }
-      } catch (error) {
-        console.error("Error al obtener las entradas:", error);
+      if (response.data.data.entries && Array.isArray(response.data.data.entries)) {
+        setEntries(response.data.data.entries.reverse());
+      } else {
+        console.error(
+          "La respuesta del servidor no contiene un array de entradas:",
+          response.data
+        );
       }
-    };
-
-    fetchData(); // Llama a fetchData() cuando se monta el componente
-  }, []);
-
-  const handleCommentAdded = (entryId) => {
-    // Puedes agregar aquí la lógica que deseas después de que se agrega un comentario
-    // Por ejemplo, puedes actualizar la lista de comentarios o hacer cualquier otra acción necesaria.
-    console.log(`Comentario agregado para la entrada con ID: ${entryId}`);
+    } catch (error) {
+      console.error("Error al obtener las entradas:", error);
+    }
   };
+
+  fetchData();
+}, [search]);
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Buscar entradas"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
       {entries.map((entry, index) => (
         <div key={`entry_${entry.id}_${index}`}>
           <UserAvatar userId={entry.userId} />
-          <div>Nombre de Usuario: {entry.owner}</div>
+          <div>Nombre de Usuario: {entry.username}</div>
           <div>
             Fotos Subidas:
             {entry.photos &&
-              JSON.parse(entry.photos).map((photoUrl, photoIndex) => (
+              entry.photos.map((photo, photoIndex) => (
                 <img
                   key={`photo_${entry.id}_${photoIndex}`}
-                  src={`${IMAGES_URL}/${photoUrl}`}
+                  src={`${IMAGES_URL}/${photo.photoName}`}
                   alt={`Foto ${photoIndex + 1}`}
                 />
               ))}
           </div>
-          <EntryLikes key={entry.id} entry={entry} />
           <div>Descripción: {entry.description}</div>
-          <ViewComments comments={entry.comments} entryId={entry.id}/>
-          <EntryComment
-            entryId={entry.id}
-            onCommentAdded={() => handleCommentAdded(entry.id)}
-          />
         </div>
       ))}
     </div>
@@ -73,5 +66,3 @@ function EntryList({ entriesProp }) {
 }
 
 export default EntryList;
-
-
